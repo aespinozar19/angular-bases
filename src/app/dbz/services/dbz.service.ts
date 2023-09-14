ffdf34a14eeba4d66efe81aead269ddb8dc99363
Anthony Espinoza <aespinozar19@gmail.com>
2023-09-13 19:16:56 -0500

    fin de seccion 5

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+import { v4 as uuid } from 'uuid';
+
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+
+
+  public characters: Character[] = [{
+    id: uuid(),
+    name: 'Krillin',
+    power: 1000
+  }, {
+    id: uuid(),
+    name: 'Goku',
+    power: 9500
+  },
+  {
+    id: uuid(),
+    name: 'Piccolo',
+    power: 4500
+  },
+  {
+    id: uuid(),
+    name: 'Vegeta',
+    power: 15450
+  }];
+
+  addCharacter( character: Character): void {
+    // let copiaCharacter=Object.assign({},character)
+
+    const newCharacter: Character = {
+      id : uuid(), ...character
+    }
+
+    this.characters.push(newCharacter);
+  }
+
+  // onDeleteCharacter( index: number): void {
+  //   this.characters.splice(index, 1)
+  // }
+
+  deleteCharacterById( id: string): void {
+    this.characters = this.characters.filter(  character => character.id !== id);
+  }
+
+}
